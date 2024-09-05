149abf03bf80432559b5efdfa703378f37c8d60c
dimadem <iamdimadem@gmail.com>
2024-09-05 16:47:08 +0100

    fix: builtin_exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -30,7 +30,7 @@ int builtin_exit(t_ms_data *data)
     int     number;
 
 	number = 0;
-    if (ft_isnumber(data->args[1]) == 0)
+    if (data->args[1] && ft_isnumber(data->args[1]) == 0)
     {
 		exit_status_handler(data, NUMERIC_REQUIRED, ft_strjoin("exit: ", data->args[1]));
         exit(NUMERIC_REQUIRED);
@@ -40,12 +40,17 @@ int builtin_exit(t_ms_data *data)
 		exit_status_handler(data, TOO_MANY_ARGS, "exit");
         exit(TOO_MANY_ARGS);
     }
-    else
+    else if (data->args[1])
     {
         number = ft_atoi(data->args[1]);
         ft_putendl_fd("exit", STDOUT_FILENO);
 		data->exit_status = number;
         set_shell_var(&data->shell_variables, "?", ft_itoa(data->exit_status));
         exit(number);
+    }
+    else
+    {
+        ft_putendl_fd("exit", STDOUT_FILENO);
+        exit(0);
     }
 }
