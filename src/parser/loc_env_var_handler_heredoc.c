1dc979b801d964adad3e73d80686169a5f33f062
¨Roman <roman.mikhaylenko14@hotmail.com>
2024-09-10 01:06:05 +0100

    loc_env_var_heredoc_utils norminette passing! safe to revert to here

--- src/parser/loc_env_var_handler_heredoc.c
+++ src/parser/loc_env_var_handler_heredoc.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redirect_heredoc.c                                 :+:      :+:    :+:   */
+/*   loc_env_var_handler_heredoc.c                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmdemirk <dmdemirk@student.42london.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -20,48 +20,77 @@
 
 static char	*assemble_result(char **tokens, size_t result_len);
 char		*token_adj(char *arg);
+char		*finalize_token(char *tmp_token, char *orig_token);
+size_t		process_single_token(char **token, t_ms_data *data);
+size_t		process_tokens(char **tokens, t_ms_data *data);
 
 char	*process_and_reassemble(char *line, t_ms_data *data)
 {
 	char	**tokens;
 	size_t	result_len;
-	int		i;
-	char	*processed_token;
 	char	*result;
-	char	*tmp_token;
-	char	*new_token;
-	char	*orig_token;
 
 	tokens = ft_split(line, ' ');
 	free(line);
 	if (!tokens)
 		return (NULL);
+	result_len = process_tokens(tokens, data);
+	result = assemble_result(tokens, result_len);
+	ft_free_2d_arr(tokens);
+	return (result);
+}
+
+size_t	process_tokens(char **tokens, t_ms_data *data)
+{
+	int		i;
+	size_t	result_len;
+
 	result_len = 0;
 	i = 0;
 	while (tokens[i])
 	{
-		orig_token = tokens[i];
-		tmp_token = expand_variable(&tokens[i], data);
-		if (tmp_token)
-		{
-			processed_token = token_adj(tmp_token);
-			if (processed_token != tmp_token)
-			{
-				new_token = ft_strdup(processed_token);
-				free(tmp_token);
-			}
-			else
-				new_token = tmp_token;
-			free(processed_token);
-			free(orig_token);
-			tokens[i] = new_token;
-			result_len += ft_strlen(new_token) + 1;
-		}
+		result_len += process_single_token(&tokens[i], data);
 		i++;
 	}
-	result = assemble_result(tokens, result_len);
-	ft_free_2d_arr(tokens);
-	return (result);
+	return (result_len);
+}
+
+size_t	process_single_token(char **token, t_ms_data *data)
+{
+	char	*orig_token;
+	char	*tmp_token;
+	char	*new_token;
+	size_t	new_len;
+
+	orig_token = *token;
+	tmp_token = expand_variable(token, data);
+	new_token = finalize_token(tmp_token, orig_token);
+	*token = new_token;
+	free(orig_token);
+	new_len = ft_strlen(new_token) + 1;
+	return (new_len);
+}
+
+char	*finalize_token(char *tmp_token, char *orig_token)
+{
+	char	*processed_token;
+	char	*new_token;
+
+	if (tmp_token)
+	{
+		processed_token = token_adj(tmp_token);
+		if (processed_token != tmp_token)
+		{
+			new_token = ft_strdup(processed_token);
+			free(tmp_token);
+		}
+		else
+			new_token = tmp_token;
+		free(processed_token);
+	}
+	else
+		new_token = ft_strdup(orig_token);
+	return (new_token);
 }
 
 static char	*assemble_result(char **tokens, size_t result_len)
@@ -82,33 +111,4 @@ static char	*assemble_result(char **tokens, size_t result_len)
 		i++;
 	}
 	return (result);
-}
-
-char	*token_adj(char *arg)
-{
-	char	*ptr;
-	char	*new_arg;
-
-	if (*arg == '\"')
-	{
-		ptr = arg + 1;
-		while (*ptr && *ptr != '\"')
-		{
-			if (!ft_isdigit(*ptr))
-				return (ft_strdup(arg));
-			ptr++;
-		}
-		if (*ptr == '\0')
-		{
-			new_arg = malloc(strlen(arg) + 2);
-			if (!new_arg)
-				return (NULL);
-			ft_strcpy(new_arg, arg);
-			ft_strcat(new_arg, "\"");
-			return (new_arg);
-		}
-		if (*ptr == '\"' && *(ptr + 1) == '\0')
-			return (ft_strdup(arg));
-	}
-	return (ft_strdup(arg));
 }
